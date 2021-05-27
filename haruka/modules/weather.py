50853cdf1221f6676a992d14efa3717944bf52f6
IoDawn <75518008+IoDawn@users.noreply.github.com>
2021-05-27 11:05:37 +0700

    Update weather.py

--- haruka/modules/weather.py
+++ haruka/modules/weather.py
@@ -22,10 +22,10 @@ from pytz import country_names as cname
 from telegram import ParseMode
 from telegram.error import BadRequest
 
-from ubotindo import API_WEATHER as APPID
-from ubotindo import dispatcher
-from ubotindo.modules.disable import DisableAbleCommandHandler
-from ubotindo.modules.helper_funcs.alternate import typing_action
+from haruka import API_WEATHER as APPID
+from haruka import dispatcher
+from haruka.modules.disable import DisableAbleCommandHandler
+from haruka.modules.helper_funcs.alternate import typing_action
 
 
 @typing_action
